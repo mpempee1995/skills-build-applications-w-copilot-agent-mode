8e6a6c08796faabc9e1eb64e9f4c534de2909119
mpempee1995 <michael.b.pearson@accenture.com>
2025-12-29 14:46:35 +0000

    Add BASE_DIR and DATABASES to settings.py

--- octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/octofit_tracker/settings.py
@@ -1,5 +1,9 @@
 # Basic settings example
+from pathlib import Path
+
 import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
@@ -19,6 +23,13 @@ INSTALLED_APPS = [
 ]
 
 ROOT_URLCONF = 'octofit_tracker.urls'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 TEMPLATES = [
     {
